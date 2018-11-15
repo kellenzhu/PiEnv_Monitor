a68da6ea53735e3fcd13404ae97c8e8a6c9326dd
Kellen Zhu <kellenzhu@163.com>
2018-11-15 23:20:41 +0800

    11/15 Update

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # PiEnv-Monitor
 ## 功能
-- 使用树莓派采集环境温度将数据存放InfluxDB，Grafana读取数据并输出图标<br>
+- 使用树莓派采集环境温度将数据存放InfluxDB，Grafana读取数据并输出图表<br>
 ![](/Pictures/Final%20Grafana.png)
 
 ## 平台
-- 树莓派3B+，DHT22温度湿度传感器，Grafana(CentOS)
+- 树莓派3B+，DHT22温湿度传感器<br>
+- InfluxDB以及Grafana均安装在CentOS 7.5
+#### 1 配置InfluxDB
+```sql
+create database "data"
+create user "admin" with password "influxadmin"
+grant all privileges to admin
+```
+#### 2 配置Grafana
+- 增加InfluxDB数据源<br>
+![](/Pictures/DataSources.jpg)
+- 创建Dashboard<br>
+![](/Pictures/Create-Dashboard.jpg)
+- 添加并编辑Panel<br>
+![](/Pictures/Graph.jpg)
 
--- main.py
+++ main.py
@@ -8,12 +8,19 @@ from db_parameter import LOGIN
 
 
 class Pi(object):
-
+    """
+    SENSER: DHT模块的型号，这里使用DHT22
+    PIN: GPIO编号
+    """
     SENSER = 22
     PIN = 4
 
     @classmethod
     def get_data(cls):
+        """
+        获取温度和湿度
+        :return: 返回包含温湿度的字典用于后面调用
+        """
         humidity, temperature = dht.read_retry(cls.SENSER, cls.PIN)
         return {'temperature': temperature, 'humidity': humidity}
 
@@ -26,24 +33,29 @@ class Pi(object):
         return alarm_count
 
     @staticmethod
-    def stdout_data(data_dict):
-        print("Current Temperature is {0:0.1f}°, humidity is {1:0.1f}%"
-              .format(data_dict['temperature'], data_dict['humidity']))
-
-    @staticmethod
     def write_db(data_dict):
+        """
+        将温湿度参数以及时间戳数据写入InfluxDB中
+        :param data_dict: 将温湿度数据的字典传入方法
+        :return: 返回写入InfluxDB的结果
+        """
 
         time = datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')
+        # time为InfluxDB Table中的时间戳
+
         client = InfluxDBClient(LOGIN['host'], LOGIN['port'],
                                 LOGIN['username'], LOGIN['password'], LOGIN['db_name'])
+        # client为连接InfluxDB的方法
         data = [
             {
-                'measurement': 'rpi-dht22',
+                'measurement': 'dht22-data',
+                # 表名为dht22-data
                 'tags': {
-                    'location': 'TBI Shanghai DC',
+                    'location': 'Shanghai',
                 },
                 'time': time,
                 'fields': {
+                    # fields为dht22-data表中的列参数，这里分别存放温度和湿度参数
                     'temperature': data_dict['temperature'],
                     'humidity': data_dict['humidity']
                 }
@@ -57,6 +69,7 @@ class Pi(object):
 if __name__ == '__main__':
     run = Pi()
     interval = 60
+    # 每隔60s获取一次数据并写入数据库
     try:
         while True:
             data_res = run.get_data()
